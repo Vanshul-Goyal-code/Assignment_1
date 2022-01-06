6bd576859079cc82b60e87622bf2c3583dbb3ed2
Vanshul-Goyal-code <96768035+Vanshul-Goyal-code@users.noreply.github.com>
2022-01-06 16:52:24 +0530

    Add files via upload

---
+++ Output.py
@@ -0,0 +1,44 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========== RESTART: /Users/vanshulgoyal/Python/Assignment1.py/Q1.py ===========
+Questin 1
+
+Enter 1st no.: 50
+Enter 2nd no.: 60
+Enter 3rd no.: 70
+Average = 60.0
+
+=========== RESTART: /Users/vanshulgoyal/Python/Assignment1.py/Q2.py ===========
+Questin 2
+
+Enter your Gross Income $100000
+Enter no. of Dependents: 10
+Your tax = $12000.0
+
+=========== RESTART: /Users/vanshulgoyal/Python/Assignment1.py/Q3.py ===========
+Questin 3
+
+SID: 21105079
+Name: vanshul goyal
+Gender(F/M/U): m
+Course Name: maths
+CGPA: 9.9
+Student: [21105079, 'Vanshul Goyal', 'M', 'Maths', 9.9]
+
+=========== RESTART: /Users/vanshulgoyal/Python/Assignment1.py/Q4.py ===========
+Questin 4
+
+Enter marks of students: 34
+Enter marks of students: 78
+Enter marks of students: 23
+Enter marks of students: 56
+Enter marks of students: 11
+Sorted Marks: ['11', '23', '34', '56', '78']
+
+=========== RESTART: /Users/vanshulgoyal/Python/Assignment1.py/Q5.py ===========
+Question 5
+Part a
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+Part b
+['Red', 'Green', 'White', 'Purple', 'Yellow']
